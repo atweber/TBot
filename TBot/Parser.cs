4853810be85cd9e4b4931ba71a5b1e6499a13bef
Aleksandr Temeev <atpbroths@gmail.com>
2019-02-13 01:27:41 +0400

    Добавьте файлы проекта.

---
+++ TBot/Parser.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBot
+{
+    class Parser
+    {
+        static HttpClient httpClient = new HttpClient();
+        public static string GetTemperature(string Title)
+        {
+            try
+            {
+                string url = $"https://api.openweathermap.org" +
+                    $"/data/2.5/weather?" +
+                    $"q={Title}&units=metric&appid=197c182f49e41dcf2f5329c267b1734b";
+                string data = httpClient.GetStringAsync(url).Result;
+                dynamic r = JObject.Parse(data);
+                return $" Температура равна {r.main.temp}C\n Скорость ветра: {r.wind.speed}m/s\n Страна: {r.sys.country}";
+            }
+            catch (Exception)
+            {
+                return "ошибка запроса...";
+            }
+        }
+    }
+}
---
+++ TBot/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string token = "710427075:AAHQuR_S3pqoFOdn1bOXpm4BGT_1pYKa7gg";
+            TelegramBotClient telegramBotClient = new TelegramBotClient(token);
+            telegramBotClient.OnMessage +=
+                delegate (object sender, MessageEventArgs e)
+                {
+                    if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Sticker)
+                    {
+                        telegramBotClient.SendStickerAsync(
+                            e.Message.Chat.Id,
+                            e.Message.Sticker.FileId
+                            );
+                    }
+
+                    else if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                    {
+                        Console.WriteLine(e.Message.Text);
+                        telegramBotClient.SendTextMessageAsync(
+                        e.Message.Chat.Id,
+                        Parser.GetTemperature(e.Message.Text)
+                        );
+                    }
+                    else
+                    {
+                        telegramBotClient.SendTextMessageAsync(
+                        e.Message.Chat.Id,
+                        "i'm don't know"
+                        );
+
+                    }
+                    
+                };
+
+
+            telegramBotClient.StartReceiving();
+
+            Console.WriteLine("Какой город Вас интересует?");
+            string city = Console.ReadLine();
+            Console.WriteLine(Parser.GetTemperature(city));
+            Console.ReadKey();
+        }
+    }
+}
